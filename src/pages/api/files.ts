1ca0f27d6f941a5c7f99ff25840424e18325577d
Xayw3 <xaywe1@gmail.com>
2023-02-24 17:41:21 +0200

    update server side

--- src/pages/api/files.ts
+++ src/pages/api/files.ts
@@ -1,9 +1,6 @@
 import fs from 'fs';
-import ffprobe from 'ffprobe';
 import path from 'path';
-import ffprobeStatic from 'ffprobe-static';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { parseFile } from 'music-metadata';
 const { getAudioDurationInSeconds } = require("get-audio-duration");
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -11,21 +8,19 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const audioFiles = fs.readdirSync(folderPath);
   const data:any = [];
 
-  for (let file of audioFiles) {
-    const audioFilePath = path.join(folderPath, file);
-
-    try {
+  try {
+    for (let file of audioFiles) {
+      const audioFilePath = path.join(folderPath, file);
       const duration = (await getAudioDurationInSeconds(audioFilePath)) | 0;
       const minutes = Math.floor(duration / 60) < 10 ? `0${Math.floor(duration / 60)}` : Math.floor(duration / 60);
       const seconds = Math.floor(duration % 60) < 10 ? `0${Math.floor(duration % 60)}` : Math.floor(duration % 60);
       data.push({ name: file, duration: `${minutes}:${seconds}` });
-    } catch (e) {
-      console.error(e);
-      res.send("500");
-    }
-  };
-
- res.status(200).json(data);
+    };
+    res.status(200).json(data);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Error reading audio files");
+  }
 };
 
 export default handler;
